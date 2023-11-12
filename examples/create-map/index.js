4b8e8666e9a815c890351b83e6228cb10cd06c03
Julian <89937721+Juliiiian@users.noreply.github.com>
2023-11-12 04:16:31 +0100

    added time stats to example

--- examples/create-map/index.js
+++ examples/create-map/index.js
@@ -8,10 +8,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 fs.readFile(__dirname + '/../../test/test.map', async function (err, fileContents) {
 	let world = rustWorld.readMap(fileContents);
 
-	let image = await world.createImage();
+	const start = new Date().getTime();
+
+	const image = await world.createImage();
+
+	console.log(`Image creation time: ${(new Date().getTime() - start) / 1000} seconds`);
 
 	if (!image) throw 'image';
 
 	const buffer = Buffer.from(image.split(',')[1], 'base64');
 	fs.writeFileSync(__dirname + '/image.png', buffer);
+	console.log('Done with saving');
 });
